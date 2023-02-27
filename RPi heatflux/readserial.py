0d2c0c758e4e9a476b7665e1fa21ab3829c15c31
jackie-mcaninch <jmcaninch@hawk.iit.edu>
2023-02-27 21:04:02 +0000

    fixing encoding, error handling, and data upload issues

--- RPi heatflux/readserial.py
+++ RPi heatflux/readserial.py
@@ -12,45 +12,40 @@ import os
 
 number_of_sensors = '4' # set number of sensors connected to Compaq DAQ
 sensitivities = ['1.08', '1.09', '1.00', '1.05']    # set sensitivities for each sensor based on its calibration sheet. First one is channel 0
-temp_data_dir = '/var/tmp/temp_heatflux'
+temp_data_dir = './temp'
 
 # initialize serial port
 try:
-    serialport = serial.Serial('/dev/ttyUSB0', 9600, timeout=2) # make sure baud rate is the same
+    serialport = serial.Serial('/dev/ttyUSB0', 9600, timeout=10) # make sure baud rate is the same
+    time.sleep(3)  # wait few seconds till serial port initialized
 except serial.SerialException:
     print('Serial Port Failed.')
     while True:     # loop forever
         time.sleep(1)
-        pass
 
-time.sleep(3)  # wait few seconds till serial port initialized
 
 # send initial configuration to the DAQ to begin logging
 try:
-    serialport.write(number_of_sensors)
+    serialport.write(bytes(number_of_sensors, 'utf-8'))
     time.sleep(1.5)
     for n in range(int(number_of_sensors)):
-        serialport.write(sensitivities[n])
+        serialport.write(bytes(sensitivities[n], 'utf-8'))
         time.sleep(1.5)
-except:
-    print('Failed to write data')
-    pass
+    time.sleep(1)
+except Exception as e:
+    print(f'Failed to write data: {e}')
 
-time.sleep(1)
 
 # loop forever
 while True:
+    time.sleep(0.01)    # wait a bit so CPU doesn't choke to def
     data = serialport.read(1)   # get first byte from serial port
     n = serialport.in_waiting   # check remaining number of bytes
     if n:    # wait till data arrives and then read it
         data = data + serialport.readline()    # read one line and merge with first byte
         try:
-            file = open('%s/t.csv' % temp_data_dir,'w')
-            file.write(data)     # save data in a temp csv file
-            file.close()
-        except:
-            pass
-    else:
-        continue    # ignore bad packets or no data received
+            with open(f'{temp_data_dir}/t.csv','wb') as f:
+                f.write(data)     # save data in a temp csv file
+        except Exception as e:
+            print(f'Error opening temp file: {e}')
 
-    time.sleep(0.01)    # wait a bit so CPU doesn't choke to def
